2d4bed00f84d1703e5dc875399f4ae91b8a68ebd
iamdragos <dragos@dragos-pelling.com>
2018-06-11 19:56:28 +0300

    finished follow up link div

--- public/app.js
+++ public/app.js
@@ -15,7 +15,46 @@
 }());
 
 (function (){
+    const links = document.querySelectorAll(".nav__item");
+    const dropdownFollow = document.querySelector('.dropdown-background');
+    const nav = document.querySelector('.nav');
+    
+    function enter() {
+        this.classList.add('active-layout')
+        setTimeout(() =>{
+            if(this.classList.contains('active-layout')) {
+                this.classList.add('active-fadein');
+            }
+        }, 150);
+        dropdownFollow.classList.add('active-open');
 
-});
+        const dropdown = this.querySelector('.dropdown');
+        const dropdownPos = dropdown.getBoundingClientRect();
+        const navPos = nav.getBoundingClientRect();
+
+
+        const coords = {
+            height: dropdownPos.height,
+            width: dropdownPos.width, 
+            top: dropdownPos.top + window.scrollY,
+            left: dropdownPos.left + window.scrollX
+        };
+
+        dropdownFollow.style.setProperty('width', `${coords.width}px`);
+        dropdownFollow.style.setProperty('height', `${coords.height}px`);
+        dropdownFollow.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+      
+    }
+
+    function leave() {
+        this.classList.remove('active-layout')
+        this.classList.remove('active-fadein');
+        dropdownFollow.classList.remove('active-open');
+    }
+
+    links.forEach((link) => link.addEventListener('mouseenter', enter));
+    links.forEach((link) => link.addEventListener('mouseleave', leave));
+}());
+
 
 
